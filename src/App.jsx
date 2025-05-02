34a6587773efc18dcdb46d87ebcedffcfe673937
Sharath Chandra Addanapuri <118981558+Sharath-Chandra43@users.noreply.github.com>
2025-05-02 07:37:17 +0530

    Updated & Corrected App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './Pages/Home';
-import Products from './pages/Products';
+import Products from './Pages/Products';
 import Cart from './Pages/Cart';
 import { CartProvider } from './Context/CartContext';
 
